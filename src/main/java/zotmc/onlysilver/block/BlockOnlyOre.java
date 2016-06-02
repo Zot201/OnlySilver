8500b61ca05f4086e702476deb89b3af034ef146
Zot201 <iZothf@gmail.com>
2016-06-02 21:55:35 +0900

    Fixed BlockOnlyOre.

--- src/main/java/zotmc/onlysilver/block/BlockOnlyOre.java
+++ src/main/java/zotmc/onlysilver/block/BlockOnlyOre.java
@@ -1,20 +1,35 @@
+/*
+ * Copyright 2016 Zot201
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package zotmc.onlysilver.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.EnumWorldBlockLayer;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockOnlyOre extends Block {
 
   public BlockOnlyOre() {
-    super(Material.rock);
+    super(Material.ROCK);
   }
 
   @SideOnly(Side.CLIENT)
-  @Override public EnumWorldBlockLayer getBlockLayer() {
-    return EnumWorldBlockLayer.CUTOUT_MIPPED;
+  @Override public BlockRenderLayer getBlockLayer() {
+    return BlockRenderLayer.CUTOUT_MIPPED;
   }
 
 }
